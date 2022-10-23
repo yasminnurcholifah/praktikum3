e738229234393c7ddd9709c3f72748dce633f3f4
yasminnurcholifah <yasminnurcholifah@gmail.com>
2022-10-23 11:13:44 +0700

    first commit

---
+++ latihan2.py
@@ -0,0 +1,6 @@
+a = 8
+b = 6
+print ("variable a=",a)
+print ("variable a=",b)
+print ("hasil penjumlahan a+b=",a+b)
+
---
+++ latihan3.py
@@ -0,0 +1,14 @@
+a = input("masukkan nilai a = ")
+b = input("masukkan nilai b = ")
+
+print (" variable a = ",a)
+print (" variable b = ",b)
+
+
+a = int(a)
+b = int(b)
+
+print("hasil penjumlahan {1}+{0} = %d".format(a,b) %(a+b))
+print("hasil pembagian  {1}/{0} = %d".format(a,b) %(a/b))
+
+print("hasil penggabungan {1}&{0}=%d".format(a,b) %(a+b))
